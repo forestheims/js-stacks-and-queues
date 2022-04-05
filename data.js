e93c6f030ada7123f6389886bf9bc834d4f3088b
forestheims <forestheims@gmail.com>
2022-04-05 15:21:45 -0700

    created a LinkedListNode class. added an add function.

--- data.js
+++ data.js
@@ -1,1 +1,29 @@
 console.log("Hellow world!");
+
+class LinkedListNode {
+  #value;
+  #next;
+
+  constructor(data) {
+    this.#value = data;
+    this.#next = null;
+  }
+
+  // next is left as null until we add a new node
+  add(node) {
+    // if next is null
+    if (!this.#next) {
+      // we can set next as node
+      this.#next = node;
+      // if next is not null
+    } else {
+      // we call add on the node that already occupies next
+      this.#next.add(node);
+    }
+  }
+}
+
+const root = new LinkedListNode("A");
+const nodeB = new LinkedListNode("B");
+root.add(nodeB);
+console.log("root", root);
