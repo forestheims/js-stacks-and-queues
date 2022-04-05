d39ed62c208bba76f00d58e44c94809c1adc4810
forestheims <forestheims@gmail.com>
2022-04-05 12:57:37 -0700

    work done before lunch

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 console.log("Hello world!");
+
+class Stack {
+  list = [];
+
+  constructor(initialList) {
+    if (initialList) this.list = initialList;
+  }
+
+  // methods
+
+  // push
+  // pop
+  stackPop() {
+    return this.list.pop();
+  }
+  // peek
+  stackPeek() {
+    return this.list[this.list.length - 1];
+  }
+}
+
+const myList = new Stack([1, 2, 3]);
+
+console.log("first pop", myList.stackPop());
+
+console.log("second pop", myList.stackPop());
+console.log("third pop", myList.stackPop());
+console.log("nothing to pop", myList.stackPop());
