679ba4ea6909c359b79ae501f398446801847478
forestheims <forestheims@gmail.com>
2022-04-05 17:36:18 -0700

    new branch

---
+++ persons.js
@@ -0,0 +1,16 @@
+class PersonTreeNode {
+  constructor(person) {
+    this.value = person.name;
+    this.person = person;
+    this.left = null;
+    this.right = null;
+  }
+
+  add(node) {
+    // implemented as in previous challenge
+  }
+
+  findPerson(name) {
+    // Implement me!
+  }
+}
